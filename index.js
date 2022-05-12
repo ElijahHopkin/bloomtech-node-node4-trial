8565b134d900703dcd508fb749de0f558018b214
enhopkin@gmail.com <enhopkin@gmail.com>
2022-05-12 10:56:52 -0600

    working to follow class...

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const server = require ('./api/server.js')
+
+const PORT = process.env.PORT;
+
+if(PORT == null) {
+    console.error('no port set')
+}else{
+
+server.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+})
+}
